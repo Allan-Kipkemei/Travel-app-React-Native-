3544ac509ec375d021283b707cae1b78f1c5349c
Allan Kiplagat <allankiplagatkipkemei@gmail.com>
2023-09-08 00:20:54 +0300

    fwverferve

---
+++ Travel-App-React-Native-main/src/components/AuthContext.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function AuthContext() {
+  return (
+    <div>
+      Technical Assignents I was given to deliver withing 4-5 hrs as a technical
+      interview for a contract Experienced Software Developer adept in bringing
+      forth expertise in design, installation, testing and maintenance of
+      software systems. Equipped with a diverse and promising skill-set.
+      Proficient in various platforms, languages, and embedded systems.
+      Experienced with the latest cutting edge development tools and procedures.
+      Able to effectively self-manage during independent projects, as well as
+      collaborate as part of a productive team.
+    </div>
+  );
+}
+
+export default AuthContext
