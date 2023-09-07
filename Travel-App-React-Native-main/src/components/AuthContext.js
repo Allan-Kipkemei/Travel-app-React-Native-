d79c96dbc47dfb0729aa1e23af61176a905e4111
Allan Kiplagat <allankiplagatkipkemei@gmail.com>
2023-09-08 00:23:18 +0300

    some deletion

--- Travel-App-React-Native-main/src/components/AuthContext.js
+++ Travel-App-React-Native-main/src/components/AuthContext.js
@@ -8,9 +8,7 @@ function AuthContext() {
       forth expertise in design, installation, testing and maintenance of
       software systems. Equipped with a diverse and promising skill-set.
       Proficient in various platforms, languages, and embedded systems.
-      Experienced with the latest cutting edge development tools and procedures.
-      Able to effectively self-manage during independent projects, as well as
-      collaborate as part of a productive team.
+    
     </div>
   );
 }
